388984fd67020b628d12a53b2043ae2d6e105fb3
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-26 16:49:17 +0200

    feat: add new task function, base exercise complete

--- js/main.js
+++ js/main.js
@@ -15,14 +15,24 @@ const { createApp } = Vue
             },
             {
                 text: "Stendere i panni",
-                done: true,
+                done: false,
             },
-        ]
+        ],
+        newTask: {
+            text: "",
+            done: false,
+        }
       }
     },
     methods: {
+        // funzione per rimuovere una task
         removeTask(index) {
             this.toDoList.splice(index, 1);
+        },
+        // funzione per aggiungere una task all'inizio dell'array e svuotare il campo dell'input
+        addTask() {
+            this.toDoList.unshift(this.newTask);
+            this.newTask = '';
         }
     }
   }).mount('#app')
